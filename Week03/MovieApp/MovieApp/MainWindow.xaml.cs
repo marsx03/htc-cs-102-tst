7ba78a08415f153c8769ad84b286e8a0ffba4243
Marissa K. <kuehnmarissa@gmail.com>
2019-10-09 19:31:02 -0400

    Created Movie class & Movie UI

---
+++ Week03/MovieApp/MovieApp/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MovieApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Movie submitted = new Movie();
+
+            submitted.title = givenTitle.Text;
+            submitted.genre = givenGenre.Text;
+            submitted.reviewScore = double.Parse(givenReviewScore.Text);
+
+        }
+    }
+}
---
+++ Week03/MovieApp/MovieApp/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApp
+{
+    class Movie
+    {
+        public string title;
+        public string genre;
+        public double reviewScore;
+        public List<string> actors;
+
+        public void Watch()
+        {
+            //do stuff
+        }
+    }
+}
