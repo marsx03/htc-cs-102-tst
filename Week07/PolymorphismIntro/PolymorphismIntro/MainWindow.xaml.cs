08229072089bf0ee96fc053be31b38b4f2daf239
Marissa K. <kuehnmarissa@gmail.com>
2019-11-06 19:26:00 -0500

    Half-finished week 7

--- Week07/PolymorphismIntro/PolymorphismIntro/MainWindow.xaml.cs
+++ Week07/PolymorphismIntro/PolymorphismIntro/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,54 +21,32 @@ namespace PolymorphismIntro
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Animal> Animals;
+
         public MainWindow()
         {
             InitializeComponent();
+            Animals = new ObservableCollection<Animal>();
+
+            Frog frog = new Frog(69,"Bort",true);
+            Dog dog = new Dog("???", 3, "[REDACTED]");
+            Duck duck = new Duck(400, "Matt");
+
+            Animals.Add(frog);
+            Animals.Add(dog);
+            Animals.Add(duck);
+
+            lvAnimals.ItemsSource = Animals;
+
         }
 
-        private void Bark_Button_Click(object sender, RoutedEventArgs e)
-        {
-            Dog dog = new Dog();
-            dog.Bark();
-        }
-
-        private void Quack_Button_Click(object sender, RoutedEventArgs e)
-        {
-            Duck duck = new Duck();
-            duck.Quack();
-        }
-
-        private void Ribbit_Button_Click(object sender, RoutedEventArgs e)
-        {
-            Frog frog = new Frog();
-            frog.Ribbit();
-        }
 
         private void SayName_Button_Click(object sender, RoutedEventArgs e)
         {
-            // Define a new List of dogs
-            List<Dog> dogs = new List<Dog>();
-            
-            // Instantiate some dog objects
-            Dog dog1 = new Dog("Muppet", 20, "Rolf");
-            Dog dog2 = new Dog("Golden Retriever", 30, "Air Bud");
-
-            // Add the dogs to the list
-            dogs.Add(dog1);
-            dogs.Add(dog2);
-
-            // Loop through the list and call a method on the objects
-            foreach (Dog d in dogs)
+           foreach(Animal x in Animals)
             {
-                d.SayName();
-            }
-
-            // Ducks and Frogs
-            Duck duck1 = new Duck(9, "Donald");
-            duck1.SayName();
-
-            Frog frog1 = new Frog(4, "Kermit", false);
-            frog1.SayName();
+                x.SayName();
+            } 
         }
     }
 }
